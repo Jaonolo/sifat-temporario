3fffdf85a8d76c508675582d982c3d4742dcbeee
LeonardoJusto <leonardo.justo@sifat.com.br>
2022-08-30 11:02:12 -0300

    WYCHF-457 https://sifatsistemas.atlassian.net/browse/WYCHF-457 AUTOATENDIMENTO - Configurações para cancelamento e reimpressao do TEF (TELA TEF ADMINISTRATIVO) Colocado alguns pontos para ser usado na reimpressão quando implementala, mas informações documentadas nas tarefas

--- autoatendimento/lib/app/modules/home/pages/tef/administrativo_tef/administrativo_tef_controller.dart
+++ autoatendimento/lib/app/modules/home/pages/tef/administrativo_tef/administrativo_tef_controller.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
+
+import '../transacao_tef/transacao_tef_controller.dart';
 
 part 'administrativo_tef_controller.g.dart';
 
@@ -7,7 +10,16 @@ class AdministrativoTefController = AdministrativoTefBase with _$AdministrativoT
 
 abstract class AdministrativoTefBase with Store {
 
+  TransacaoTefController transacaoTefController = Modular.get();
+
   void cancelamentoTef() {
     Modular.to.pushNamed("/cancelamento_tef");
+  }
+
+  void reimpressaoTef(BuildContext context){
+    // REIMPRESSÃO NÃO FUNCIONANDO
+    Modular.to.pushNamed("/transacao");
+    transacaoTefController.comunicaWebSocket(context);
+    transacaoTefController.reimprimirTransacao();
   }
 }
